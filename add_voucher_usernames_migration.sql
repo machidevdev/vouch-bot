04034eb0353e0211a4ca6af70b31fa202b63c339
machidevdev <0xmachidev@gmail.com>
2025-09-08 17:04:35 +0800

    fix: dm vouch in staging

--- add_voucher_usernames_migration.sql
+++
@@ -1,8 +0,0 @@
--- This migration adds the voucherUsernames field to track all users who vouched
--- Run this with: npx prisma db push (for development) or create a proper migration
-
--- Add the new voucherUsernames column
-ALTER TABLE "Vote" ADD COLUMN "voucherUsernames" TEXT[] DEFAULT '{}';
-
--- Populate existing records with createdBy as the initial voucher
-UPDATE "Vote" SET "voucherUsernames" = ARRAY[createdBy] WHERE "voucherUsernames" = '{}';
